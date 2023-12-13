7b71f56c3f76733eb6480ee1f0862729886b80a8
dan <dan@mooresolutions.io>
2023-12-13 07:59:18 -0600

    vuetify changes

--- app.js
+++ app.js
@@ -1,47 +1,49 @@
-import { createClient } from 'redis'
-import { GSDispatcher } from './lib/GSDispatcher.js'
-import * as dotenv from 'dotenv'
+import { createClient } from "redis";
+import { GSDispatcher } from "./lib/GSDispatcher.js";
+import * as dotenv from "dotenv";
 
 const opts = dotenv.config({
-                               path: './.env',
-                           }).parsed;
+    path: "./.env",
+}).parsed;
 
 const client = createClient({
-                                host: opts.REDIS_HOST,
-                                port: opts.REDIS_PORT, })
+    host: opts.REDIS_HOST,
+    port: opts.REDIS_PORT,
+});
 
-client.subscribe('laravel_database_gsd', (event) => {
+client.subscribe("laravel_database_gsd", (event) => {
     try {
-        const data = JSON.parse(event)
+        const data = JSON.parse(event);
         return new Promise((resolve, reject) => {
-            const dispatcher = new GSDispatcher(data)
-            dispatcher.dispatch()
+            const dispatcher = new GSDispatcher(data);
+            dispatcher
+                .dispatch()
                 .then(() => {
-                    resolve()
-                    dispatcher.updateStatus('success')
+                    resolve();
+                    dispatcher.updateStatus("success");
                 })
                 .catch((err) => {
-                    console.log('GSDispatcher Err', err)
-                    reject(dispatcher.updateStatus('error'))
-                })
-        })
+                    console.log("GSDispatcher Err", err);
+                    resolve(); // idk k maybe?
+                });
+        });
     } catch (e) {
-        console.log('Redis Client Subscribe Err', e)
+        console.log("Redis Client Subscribe Err", e);
     }
-})
+});
 
-client.on('error', err => new GSDispatcher({
-                                               status: 'error'
-                                           }).updateStatus('error', err))
+client.on("error", (err) =>
+    new GSDispatcher({
+        status: "error",
+    }).updateStatus("error", err),
+);
 
-client.on( 'connect', async () => {
-   await new GSDispatcher({
-                         status: 'information',
-                     }).updateStatus('connected')
-})
+client.on("connect", async () => {
+    await new GSDispatcher({
+        status: "information",
+    }).updateStatus("connected");
+});
 
-await client.connect()
-    .catch(err => console.log('Redis Client Connect Err', err))
+await client.connect().catch((err) => console.log("Redis Client Connect Err", err));
 
-export default client
-
+export default client;
