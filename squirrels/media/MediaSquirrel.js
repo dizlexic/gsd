f87eb761cfd1e53c3eced8c56e34dc155dfba29b
daniel <dan@mooresolutions.io>
2023-12-24 07:21:53 -0600

    Media pull and push working are working. Needs refinement WIP

--- squirrels/media/MediaSquirrel.js
+++ squirrels/media/MediaSquirrel.js
@@ -1,57 +1,104 @@
+import { delayRandom } from '../shared/util.js';
 import Squirrel from '../shared/Squirrel.js';
+import { config } from './config.js';
+import FormData from 'form-data';
+import * as https from 'https';
 
-
+/**
+ * Media Squirrel
+ * @class
+ * @extends Squirrel
+ * @description Handles downloading media files from source and sending them to API
+ **/
+// Downloads media files from source and sends to API
 export default class MediaSquirrel extends Squirrel {
     async run() {
         console.log('Media Squirrel from index.js running')
         const task = {...this._task}
 
-        const { missing } = task?.data || { missing: [] };
+        const { media } = await this.getTargets(task);
 
-        if (missing.length === 0) {
-            console.log('Media Squirrel: no missing files')
-            return this.updateStatus(task, 'finished');
+        if (!media?.length) {
+            console.log('Media Squirrel: no media to transfer')
+            return this.updateStatus(task, 'done');
         }
 
-        const length = missing.length;
-        console.log('Media Squirrel: missing files', length)
-
-        // while(missing.length > 0) {
-        //     const missing_person = missing.pop();
-        //     console.log('Media Squirrel: missing file', file)
-        //     await delayRandom(this.config.delayRange.min, this.config.delayRange.max)
-        //
-        //     const file = {
-        //         person: missing_person,
-        //         type: 'photo',
-        //         url: missing_person?.photo,
-        //     }
-        //
-        //     await this.download(file);
-        // }
+        for (const file of media) {
+            await delayRandom(config.delayRange.min, config.delayRange.max);
+            console.log('Media Squirrel: transferring file', file?.id);
+            await this.handle(file);
+        }
     }
 
-    async download(file)
+    async getTargets() {
+        const url = new URL(`${config.apiServer}${config.targetEndpoint}`);
+        url.searchParams.append('limit', config.limit);
+        console.log('Media Squirrel: getting targets', url)
+        const res = await this._xhttp.get(url.href, {timeout: config.timeout});
+        console.log(`Media Squirrel: received ${res?.data?.media?.length ?? 0} targets`)
+        return res?.data
+    }
+
+    // Download file and send to API
+    // Sends file as multipart/form-data
+    // Sends task as JSON
+    // Returns response from API
+    // TODO: handle errors
+    async handle(file)
     {
-        console.log('Media Squirrel: downloading file', file)
+        return new Promise((resolve, reject) => {
+            // set task data
+            const task = {...this._task};
+            task.data = { file };
+            task.file = file;
 
-        const url = new URL(file)
+            delete task?.dispatcher;
+            delete task?._manager;
 
-        try {
-            const res = await this.xhttp.get(file);
-        } catch (err) {
-            console.log('Media Squirrel: error downloading file', file, err)
-            missing.push(file);
-            return this.updateStatus(task, 'error');
-        }
-    }
+            const form = new FormData();
+            // Initiate download
+            https.get(task.file?.source, response => {
 
-    async streamFile(file) {
+                // Configure form and append data
+                form.setBoundary('----SquirrelFormBoundary7MA4YWxkTrZu0gW')
+                form.append('task', JSON.stringify(task))
+                form.append('file', response); // file stream
 
+                const headers = {
+                    'Authorization': 'Bearer ' + task?.token,
+                    'Connection': 'keep-alive',
+                    'Cache-Control': 'no-cache',
+                    'Transfer-Encoding': 'chunked',
+                    'Accept': 'application/json',
+                    'Content-type': `multipart/form-data; boundary=${form.getBoundary()}`,
+                    'User-Agent': 'Squirrel/1.0.0',
+                }
+
+                // Send file to API
+                form.submit(
+                {
+                    host: 'localhost',
+                    path: '/api/media/update/' + task.file?.id,
+                    auth: task?.token,
+                    headers,
+                }, (error, response) => {
+                    if (error) reject(error);
+                    let body = '';
+                    if (!response) return reject('No response from server?');
+                    response.on('data', chunk => {
+                        body += chunk.toString()
+                    });
+                    response.on('end', () => {
+                        resolve(JSON.parse(body))
+                    });
+                    response.resume();
+                });
+            });
+        });
     }
 
     async save(tries = 0) {
         const task = {...this._task};
-        console.log('Saving Media Squirrel task', task);
+        console.log(`Media Squirrel: NOOP save ${task?.id} completed`);
     }
 }
--- squirrels/media/config.js
+++ squirrels/media/config.js
@@ -3,6 +3,12 @@ export const config = {
         min: process.env?.DELAYRANGE_MIN || 10000,
         max: process.env?.DELAYRANGE_MIN || 20000,
     },
+
+    apiServer: process.env.MEDIA_API_SERVER || 'http://localhost',
+    targetEndpoint: process.env.MEDIA_ENDPOINT || '/api/media/unsaved',
+    limit: process.env.MEDIA_LIMIT || 10,
+    timeout: process.env.MEDIA_TIMEOUT || 10000,
+    uploadEndpoint: process.env.MEDIA_UPLOAD_ENDPOINT || '/api/media/update',
 };
 
 export default config;
