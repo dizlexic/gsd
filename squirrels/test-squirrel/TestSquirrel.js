a35165d6ec61f7954f47feff27cbf0afdb467dee
dan <dan@mooresolutions.io>
2024-01-03 13:39:09 -0600

    Starting to stub out squirrels admin pages.

--- squirrels/test-squirrel/TestSquirrel.js
+++ squirrels/test-squirrel/TestSquirrel.js
@@ -1,16 +1,25 @@
 import Squirrel from '../../lib/Squirrel.js';
 import { delay } from '../../shared/util.js';
 
-export default class TestSquirrel extends Squirrel {
-    async run() {
-        console.log('running')
-        await delay(1000)
-        console.log('ran')
+export default class TestSquirrel extends Squirrel
+{
+    async run()
+    {
+        console.log('running');
+        this.notifi.updateStatus('running');
+        await delay(1000);
+        this.notifi.updateStatus('ran');
+        console.log('ran');
     }
 
-    async save() {
-        console.log('saving')
-        await delay(1000)
-        console.log('saved')
+
+    async save()
+    {
+        this.notifi.updateStatus('saving');
+        await delay(1000);
+        this.notifi.updateStatus('saved');
+        this.notifi.complete();
     }
+
+
 }
